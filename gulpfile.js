9b2b1da1d9f48633dffaf048e16b9bd84cfe7f74
Nidhi Bhatt <Nidhibhatt@promact.com>
2017-09-14 16:19:33 +0530

    add amaze

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+gulp.task('sass', function () {
+    return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+})
+gulp.watch('app/scss/**/*.scss', ['sass']); 
+gulp.task('watch', function () {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    // Other watchers
+})
+gulp.task('sass', function () {
+    return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    })
+})
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+});
